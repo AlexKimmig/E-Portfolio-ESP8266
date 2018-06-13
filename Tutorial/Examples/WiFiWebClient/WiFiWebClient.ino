e6b5a993b08fea44ccad02710ac3be68352c9ee1
KimmigAlex <Kimmig.Alex@gmail.com>
2018-06-13 16:48:19 +0200

    Finished presentation, added examples

---
+++ Tutorial/Examples/WiFiWebClient/WiFiWebClient.ino
@@ -0,0 +1,94 @@
+#include <ESP8266WiFi.h>
+
+char ssid[] = "honor 9"; //  your network SSID (name)
+char pass[] = "passwort";    // your network password (use for WPA, or use as key for WEP)
+
+int status = WL_IDLE_STATUS;
+char server[] = "api.openweathermap.org";    // name address for Google (using DNS)
+
+// Initialize the Ethernet client library
+// with the IP address and port of the server
+// that you want to connect to (port 80 is default for HTTP):
+WiFiClient client;
+
+void setup() {
+  //Initialize serial and wait for port to open:
+  Serial.begin(9600);
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for native USB port only
+  }
+
+  testWiFi(ssid, pass);
+
+  printWifiStatus();
+
+  Serial.println("\nStarting connection to server...");
+  // if you get a connection, report back via serial:
+  if (client.connect(server, 80)) {
+    Serial.println("connected to server");
+    // Make a HTTP request:
+    client.println("GET /data/2.5/weather?id=2892794&units=metric&APPID=8b0cf6148353f8a2f51e348222ca6bb1 HTTP/1.1");
+    client.println("Host: www.google.com");
+    client.println("Connection: close");
+    client.println();
+  }
+}
+
+void loop() {
+  // if there are incoming bytes available
+  // from the server, read them and print them:
+  while (client.available()) {
+    char c = client.read();
+    Serial.write(c);
+  }
+
+  // if the server's disconnected, stop the client:
+  if (!client.connected()) {
+    Serial.println();
+    Serial.println("disconnecting from server.");
+    client.stop();
+
+    // do nothing forevermore:
+    while (true);
+  }
+}
+
+
+void printWifiStatus() {
+  // print the SSID of the network you're attached to:
+  Serial.print("SSID: ");
+  Serial.println(WiFi.SSID());
+
+  // print your WiFi shield's IP address:
+  IPAddress ip = WiFi.localIP();
+  Serial.print("IP Address: ");
+  Serial.println(ip);
+
+  // print the received signal strength:
+  long rssi = WiFi.RSSI();
+  Serial.print("signal strength (RSSI):");
+  Serial.print(rssi);
+  Serial.println(" dBm");
+}
+
+boolean testWiFi(char ssid[], char pass[]) {
+  //Check connection
+  int ctr = 0;
+  WiFi.begin(ssid, pass);
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(1500);
+    ctr++;
+    if (ctr == 10) {
+      WiFi.disconnect();
+      return false;
+    }
+  }
+  Serial.println();
+  return true;
+}
+
+
+
+
+
